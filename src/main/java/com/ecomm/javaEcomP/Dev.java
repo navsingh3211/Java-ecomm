d7971c7028db8250b255685c2877f66c25a1f5a4
navsingh3211 <navsingh3211@gmail.com>
2024-11-08 18:05:34 +0530

    dd

--- src/main/java/com/ecomm/javaEcomP/Dev.java
+++ src/main/java/com/ecomm/javaEcomP/Dev.java
@@ -1,5 +1,11 @@
 package com.ecomm.javaEcomP;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Dev {
-    
+
+    public void build(){
+        System.out.println("hi bro how re u");
+    }
 }
--- src/main/java/com/ecomm/javaEcomP/JavaEcomPApplication.java
+++ src/main/java/com/ecomm/javaEcomP/JavaEcomPApplication.java
@@ -2,12 +2,16 @@ package com.ecomm.javaEcomP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class JavaEcomPApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaEcomPApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(JavaEcomPApplication.class, args);
+		Dev obj = context.getBean(Dev.class);
+		obj.build();
 	}
 
 }
